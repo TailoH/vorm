f2a03074bf0eb786ca8a1276f322cb67c92b8aa1
Siim <siim.aarmaa@hotmail.com>
2015-10-30 09:58:50 +0200

    Create saada2.php

---
+++ saada2.php
@@ -0,0 +1,24 @@
+<?php
+
+$item = json_decode(file_get_contents("data.txt"), 2);
+$item = $item[$_GET["key"]];
+
+?>
+
+<?php
+
+//Load the file
+$contents = file_get_contents('data.txt');
+
+//Decode the JSON data into a PHP array.
+$contentsDecoded = json_decode($contents, true);
+
+//Modify the counter variable.
+$contentsDecoded['counter']++;
+
+//Encode the array back into a JSON string.
+$json = json_encode($contentsDecoded);
+
+//Save the file.
+file_put_contents('data.txt', $json);
+?>
